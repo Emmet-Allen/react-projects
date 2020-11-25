2d5ba61356dd1829aa03c8111ab470f158c2378c
Emmet Allen <59685332+Mrskillful@users.noreply.github.com>
2020-11-24 20:33:20 -0500

    Inital Commit

--- 1-birthday-reminder/setup/src/List.js
+++ 1-birthday-reminder/setup/src/List.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
+import data from "./data";
 
 const List = () => {
+  const [people, setPeople] = useState(data);
+
+  const removePerson = (id) => {
+    let removed = people.filter((person) => person.id !== id);
+    setPeople(removed);
+  };
+
   return (
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name}></img>
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+              <button className="btn" onClick={() => removePerson(id)}>
+                Remembered!
+              </button>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
